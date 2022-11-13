a7d03c93bbeac800b086eb9e955f064c4998ac54
rnath <radhika.nath@ukaea.uk>
2022-11-13 21:12:12 +0000

    Refactored train_ggcnn.py to understand lightning

--- train_ggcnn.py
+++ train_ggcnn.py
@@ -77,38 +77,39 @@ def validate(net, device, val_data, batches_per_epoch):
     ld = len(val_data)
 
     with torch.no_grad():
-        batch_idx = 0
-        while batch_idx < batches_per_epoch:
-            for x, y, didx, rot, zoom_factor in val_data:
-                batch_idx += 1
-                if batches_per_epoch is not None and batch_idx >= batches_per_epoch:
-                    break
+        # batch_idx = 0
+        # while batch_idx < batches_per_epoch:
+        for batch_idx, batch in enumerate(val_data):
+            x, y, didx, rot, zoom_factor = batch
+            # batch_idx += 1
+            if batches_per_epoch is not None and batch_idx >= batches_per_epoch:
+                break
 
-                xc = x.to(device)
-                yc = [yy.to(device) for yy in y]
-                lossd = net.compute_loss(xc, yc)
+            xc = x.to(device)
+            yc = [yy.to(device) for yy in y]
+            lossd = net.compute_loss(xc, yc)
 
-                loss = lossd['loss']
+            loss = lossd['loss']
 
-                results['loss'] += loss.item()/ld
-                for ln, l in lossd['losses'].items():
-                    if ln not in results['losses']:
-                        results['losses'][ln] = 0
-                    results['losses'][ln] += l.item()/ld
+            results['loss'] += loss.item()/ld
+            for ln, l in lossd['losses'].items():
+                if ln not in results['losses']:
+                    results['losses'][ln] = 0
+                results['losses'][ln] += l.item()/ld
 
-                q_out, ang_out, w_out = post_process_output(lossd['pred']['pos'], lossd['pred']['cos'],
-                                                            lossd['pred']['sin'], lossd['pred']['width'])
+            q_out, ang_out, w_out = post_process_output(lossd['pred']['pos'], lossd['pred']['cos'],
+                                                        lossd['pred']['sin'], lossd['pred']['width'])
 
-                s = evaluation.calculate_iou_match(q_out, ang_out,
-                                                   val_data.dataset.get_gtbb(didx, rot, zoom_factor),
-                                                   no_grasps=1,
-                                                   grasp_width=w_out,
-                                                   )
+            s = evaluation.calculate_iou_match(q_out, ang_out,
+                                               val_data.dataset.get_gtbb(didx, rot, zoom_factor),
+                                               no_grasps=1,
+                                               grasp_width=w_out,
+                                               )
 
-                if s:
-                    results['correct'] += 1
-                else:
-                    results['failed'] += 1
+            if s:
+                results['correct'] += 1
+            else:
+                results['failed'] += 1
 
     return results
 
@@ -133,44 +134,47 @@ def train(epoch, net, device, train_data, optimizer, batches_per_epoch, vis=Fals
 
     net.train()
 
-    batch_idx = 0
+    # batch_idx = 0
     # Use batches per epoch to make training on different sized datasets (cornell/jacquard) more equivalent.
-    while batch_idx < batches_per_epoch:
-        for x, y, _, _, _ in train_data:
-            batch_idx += 1
-            if batch_idx >= batches_per_epoch:
-                break
+    # while batch_idx < batches_per_epoch:
+    for batch_idx, batch in enumerate(train_data):
+        # print(batch_idx)
+        # batch_idx += 1
+        if batch_idx >= batches_per_epoch:
+            # print("breaking")
+            break
+        x, y, _, _, _ = batch
+        xc = x.to(device)
+        yc = [yy.to(device) for yy in y]
+        lossd = net.compute_loss(xc, yc)
 
-            xc = x.to(device)
-            yc = [yy.to(device) for yy in y]
-            lossd = net.compute_loss(xc, yc)
+        loss = lossd['loss']
 
-            loss = lossd['loss']
+        if batch_idx % 100 == 0:
+            logging.info('Epoch: {}, Batch: {}, Loss: {:0.4f}'.format(epoch, batch_idx, loss.item()))
 
-            if batch_idx % 100 == 0:
-                logging.info('Epoch: {}, Batch: {}, Loss: {:0.4f}'.format(epoch, batch_idx, loss.item()))
+        results['loss'] += loss.item()
+        for ln, l in lossd['losses'].items():
+            if ln not in results['losses']:
+                results['losses'][ln] = 0
+            results['losses'][ln] += l.item()
 
-            results['loss'] += loss.item()
-            for ln, l in lossd['losses'].items():
-                if ln not in results['losses']:
-                    results['losses'][ln] = 0
-                results['losses'][ln] += l.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # Display the images
-            if vis:
-                imgs = []
-                n_img = min(4, x.shape[0])
-                for idx in range(n_img):
-                    imgs.extend([x[idx,].numpy().squeeze()] + [yi[idx,].numpy().squeeze() for yi in y] + [
-                        x[idx,].numpy().squeeze()] + [pc[idx,].detach().cpu().numpy().squeeze() for pc in lossd['pred'].values()])
-                gridshow('Display', imgs,
-                         [(xc.min().item(), xc.max().item()), (0.0, 1.0), (0.0, 1.0), (-1.0, 1.0), (0.0, 1.0)] * 2 * n_img,
-                         [cv2.COLORMAP_BONE] * 10 * n_img, 10)
-                cv2.waitKey(2)
+        # Display the images
+        if vis:
+            imgs = []
+            n_img = min(4, x.shape[0])
+            for idx in range(n_img):
+                imgs.extend([x[idx,].numpy().squeeze()] + [yi[idx,].numpy().squeeze() for yi in y] + [
+                    x[idx,].numpy().squeeze()] + [pc[idx,].detach().cpu().numpy().squeeze() for pc in
+                                                  lossd['pred'].values()])
+            gridshow('Display', imgs,
+                     [(xc.min().item(), xc.max().item()), (0.0, 1.0), (0.0, 1.0), (-1.0, 1.0), (0.0, 1.0)] * 2 * n_img,
+                     [cv2.COLORMAP_BONE] * 10 * n_img, 10)
+            cv2.waitKey(2)
 
     results['loss'] /= batch_idx
     for l in results['losses']:
@@ -201,7 +205,8 @@ def run():
 
     train_dataset = Dataset(args.dataset_path, start=0.0, end=args.split, ds_rotate=args.ds_rotate,
                             random_rotate=True, random_zoom=True,
-                            include_depth=args.use_depth, include_rgb=args.use_rgb)
+                            include_depth=args.use_depth, include_rgb=args.use_rgb,
+                            increase_data_length=True, data_length_increase=10000)
     train_data = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.batch_size,
@@ -210,7 +215,8 @@ def run():
     )
     val_dataset = Dataset(args.dataset_path, start=args.split, end=1.0, ds_rotate=args.ds_rotate,
                           random_rotate=True, random_zoom=True,
-                          include_depth=args.use_depth, include_rgb=args.use_rgb)
+                          include_depth=args.use_depth, include_rgb=args.use_rgb,
+                          increase_data_length=True, data_length_increase=10000)
     val_data = torch.utils.data.DataLoader(
         val_dataset,
         batch_size=1,
